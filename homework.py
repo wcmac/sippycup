ed0c43b9820d2eecc5973cbe16f5b5ab8e0392c6
Bill MacCartney <wcmac@cs.stanford.edu>
2016-04-09 21:56:35 -0700

    Migrated SippyCup codebase from Python 2 to Python 3.

--- homework.py
+++ homework.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python
+
 # coding: utf-8
+
+# In[ ]:
+
+#!/usr/bin/env python
+
+
 
 # # [CS224U](https://web.stanford.edu/class/cs224u/) Homework 5
 # 
-# This homework is distributed in three content-identical formats (html, py,
-# ipynb) as part of the SippyCup codebase. All seven problems are required.
-# You're encouraged to turn your work in as iPython HTML output or as a Python
-# script. This work is due by the start of class on May 16.
+# This homework is distributed in three content-identical formats (html, py, ipynb) as part of the SippyCup codebase. All seven problems are required. You're encouraged to turn your work in as iPython HTML output or as a Python script. This work is due by the start of class on May 16.
 # 
-# Be sure to put this code, or run this notebook, inside [the SippyCup
-# codebase](https://github.com/wcmac/sippycup).
+# Be sure to put this code, or run this notebook, inside [the SippyCup codebase](https://github.com/wcmac/sippycup).
 # 
 # * [Arithmetic domain](#Arithmetic-domain)
 #   * [Question 1](#Question-1)
@@ -25,10 +27,9 @@
 #   * [Question 7](#Question-7)
 
 # ## Arithmetic domain
-#
-# SippyCup includes a module `arithmetic` with a class
-# `ArithmeticDomain` that brings together the examples from unit 1.
-# Here's an example of the sort of use you'll make of this domain
+# 
+# SippyCup includes a module `arithmetic` with a class `ArithmeticDomain` that brings together
+# the examples from unit 1. Here's an example of the sort of use you'll make of this domain
 # for these homework problems.
 
 # In[ ]:
@@ -49,8 +50,7 @@ for parse in parses:
     print('Denotation:', math_domain.execute(parse.semantics))
 
 
-# This is a convenience function we'll use for seeing what the grammar
-# is doing:
+# This is a convenience function we'll use for seeing what the grammar is doing:
 
 # In[ ]:
 
@@ -66,9 +66,8 @@ def display_examples(utterances, grammar=None, domain=None):
 
 # ### Question 1
 # 
-# Your task is to extend `ArithmeticDomain` to include the unary
-# operators `squared` and `cubed`, which form expressions like `three
-# squared` and `nine cubed`. The following code will help
+# Your task is to extend `ArithmeticDomain` to include the unary operators `squared` and `cubed`, 
+# which form expressions like `three squared` and `nine cubed`. The following code will help
 # you get started on this. __Submit__: your completion of this code.
 
 # In[ ]:
@@ -95,19 +94,15 @@ display_examples(('three squared', 'minus three squared', 'four cubed'),
 
 # ### Question 2
 # 
-# Your task is to extend `ArithmeticDomain` to support numbers with
-# decimals, so that you can parse all expressions of the form `N point
-# D` where `N` and `D` both denote `int`s. You can assume that both
-# numbers are both spelled out as single words, as in "one point
-# twelve" rather than "one point one two". (The grammar fragment has
-# only "one", "two", "three", and "four" anyway.) __Submit__: your
-# completion of the following code.
+# Your task is to extend `ArithmeticDomain` to support numbers with decimals, so 
+# that you can parse all expressions of the form `N point D` where `N` and `D` both denote
+# `int`s. You can assume that both numbers are both spelled out as single words, as in
+# "one point twelve" rather than "one point one two". (The grammar fragment has only "one",
+# "two", "three", and "four" anyway.) __Submit__: your completion of the following code.
 # 
-# __Important__: your grammar should not create spurious parses like
-# `(two times three) point four`. This means that you can't treat
-# `point` like the other binary operators in your syntactic grammar.
-# This will require you to add special rules to handle the internal
-# structure of these decimal numbers.
+# __Important__: your grammar should not create spurious parses like `(two times three) point four`. 
+# This means that you can't treat `point` like the other binary operators in your syntactic grammar. 
+# This will require you to add special rules to handle the internal structure of these decimal numbers.
 
 # In[ ]:
 
@@ -146,11 +141,11 @@ display_examples(('four point two', 'minus four point one', 'two minus four poin
 # ### Question 3
 # 
 # Extend the grammar to support the multi-word expression `the average of` as 
-# in `the average of one and four`. Your solution is required to treat
-# each word in this multi-word expression as its own lexical item.
-# Other than that, you have a lot of design freedom. Your solution can
-# be limited to the case where the conjunction consists of just two
-# numbers. __Submit__: your completion of this starter code.
+# in `the average of one and four`.
+# Your solution is required to treat each word in this multi-word expression as its own lexical item.
+# Other than that, you have a lot of design freedom.
+# Your solution can be limited to the case where the conjunction consists of just two numbers.
+# __Submit__: your completion of this starter code.
 
 # In[ ]:
 
@@ -172,8 +167,7 @@ display_examples(('the one', 'the average of one and four'),
 
 # ## Travel domain
 
-# Here's an illustration of how parsing and interpretation work in
-# this domain:
+# Here's an illustration of how parsing and interpretation work in this domain:
 
 # In[ ]:
 
@@ -224,17 +218,15 @@ train_test(
 
 # ### Question 4
 # 
-# With the default travel grammar, many of the errors on training
-# examples occur because the origin isn't marked by "from". You might
-# have noticed that "directions New York to Philadelphia" is not
-# handled properly in our opening example. Other examples include
-# "transatlantic cruise southampton to tampa", "fly boston to myrtle
-# beach spirit airlines", and "distance usa to peru". __Your tasks__:
-# (i) extend the grammar with a single rule to handle examples like
-# these, and run another evaluation using this expanded grammar
-# (submit your completion of the following starter code); (ii) in
-# 1&ndash;2 sentences, summarize what happened to the post-training
-# performance metrics when this rule was added.
+# With the default travel grammar, many of the errors on training examples occur because the origin 
+# isn't marked by "from". You might have noticed that "directions New York to Philadelphia"
+# is not handled properly in our opening example. Other examples include 
+# "transatlantic cruise southampton to tampa", 
+# "fly boston to myrtle beach spirit airlines", and 
+# "distance usa to peru". __Your tasks__: (i) extend the grammar with a single rule to handle examples 
+# like these, and run another evaluation using this expanded grammar (submit your completion
+# of the following starter code); (ii) in 1&ndash;2 sentences,
+# summarize what happened to the post-training performance metrics when this rule was added.
 
 # In[ ]:
 
@@ -259,20 +251,17 @@ train_test(
 
 # ### Question 5
 # 
-# Your extended grammar for question 4 likely did some harm to the
-# space of parses we consider. Consider how `number of parses` has
-# changed. (If it's not intuitively clear why, check out some of the
-# parses to see what's happening!)
+# Your extended grammar for question 4 likely did some harm to the space of parses we 
+# consider. Consider how `number of parses` has changed. (If it's not intuitively clear why, 
+# check out some of the parses to see what's happening!)
 # 
-# __Your task__: to try to make amends, expand the feature function to
-# improve the ability of the optimizer to distinguish good parses from
-# bad. You can write your own function and/or combine it with scoring
-# functions that are available inside SippyCup. You should be able to
-# achieve a gain in post-training train and test `semantics accuracy`.
-# (Note: you should _not_ spend hours continualy improving your score
-# unless you are planning to develop this into a project. Any gain over
-# the previous run will suffice here.) __Submit__: your completion of
-# this code.
+# __Your task__: to try to make amends, expand the feature function to improve the 
+# ability of the optimizer to distinguish good parses from bad. You can write your
+# own function and/or combine it with scoring functions that are available inside
+# SippyCup. You should be able to achieve a gain in post-training train and
+# test `semantics accuracy`. (Note: you should _not_ spend hours continually improving 
+# your score unless you are planning to develop this into a project. Any gain over
+# the previous run will suffice here.) __Submit__: your completion of this code.
 
 # In[ ]:
 
@@ -291,7 +280,7 @@ train_test(
 
 # ## GeoQuery domain
 
-# Here are a few simple examples from the GeoryQuery domain:
+# Here are a few simple examples from the GeoQuery domain:
 
 # In[ ]:
 
@@ -308,10 +297,9 @@ display_examples(
     domain=geo_domain)
 
 
-# And we can train models just as we did for the travel domain, though
-# we have to be more attentive to special features of semantic parsing
-# in this domain. Here's a run with the default scoring model,
-# metrics, etc.:
+# And we can train models just as we did for the travel domain, though we have to be more
+# attentive to special features of semantic parsing in this domain. Here's a run with the
+# default scoring model, metrics, etc.:
 
 # In[ ]:
 
@@ -346,20 +334,15 @@ special_geo_evaluate(grammar=geo_grammar)
 
 # ### Question 6
 # 
-# Two deficiencies of the current grammar:
+# Two deficiencies of the current grammar: 
 # 
-# * The words "where" and "is" are treated as being of category
-# `$Optional`, which means they are ignored. As a result, the grammar
-# construes all questions of the form "where is X" as being about the
-# identity of X!
+# * The words "where" and "is" are treated as being of category `$Optional`, which means they are ignored. As a result, the grammar construes all questions of the form "where is X" as being about the identity of X!
 # 
-# * Queries like "how many people live in Florida" are not handled
-# correctly.
+# * Queries like "how many people live in Florida" are not handled correctly.
 # 
-# __Your task__: Add grammar rules that address these problems and
-# assess impact of the changes using the `train_test` based interface
-# illustrated above. __Submit__: your expanded version of the starter
-# code below.
+# __Your task__: Add grammar rules that address these problems and assess impact of the changes
+# using the `train_test` based interface illustrated above. 
+# __Submit__: your expanded version of the starter code below.
 
 # In[ ]:
 
@@ -377,13 +360,8 @@ special_geo_evaluate(grammar=geo_grammar)
 
 # ### Question 7
 # 
-# The success of the `empty_denotation` feature demonstrates the
-# potential of denotation features. Can we go further? Experiment with
-# a feature or features that characterize the _size_ of the denotation
-# (that is, the number of answers). This will involve extending
-# `geo_domain.features` and running another assessment. __Submit__:
-# your completion of the code below and 1&ndash;2 sentences saying how
-# this feature seems to behave in the model.
+# The success of the `empty_denotation` feature demonstrates the potential of denotation features. Can we go further?  Experiment with a feature or features that characterize the _size_ of the denotation (that is, the number of answers). This will involve extending `geo_domain.features` and running another assessment. __Submit__: your completion of the 
+# code below and 1&ndash;2 sentences saying how this feature seems to behave in the model.
 
 # In[ ]:
 
